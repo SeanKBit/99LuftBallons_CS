14665054724240d88e03766a721e191d1d2fb590
SeanKBit <seankbittner@gmail.com>
2021-09-30 13:11:46 -0700

    Update 99Program_CS.cs
    added a LINQ example

--- 99Program_CS.cs
+++ 99Program_CS.cs
@@ -10,6 +10,11 @@ namespace NinetyNineLuftBalloons
     {
         static void Main(string[] args)
         {
+            List<string> coordList = new List<string>();
+            int xCoord;
+            int yCoord;
+            string newString;
+
             Console.SetWindowSize(60, 40);
 
             for (int i = 0; i < 100; i++)
@@ -20,6 +25,13 @@ namespace NinetyNineLuftBalloons
 
                 // Create new Balloon object
                 Balloon bImage = new Balloon();
+
+                // Use getter to add coords to list
+                xCoord = bImage.IntX;
+                yCoord = bImage.IntY;
+                newString = $"{xCoord.ToString()},{yCoord.ToString()}";
+                coordList.Add(newString);
+
                 bImage.show();
 
                 if (is99(i)) // If 99 balloons, celebrate...
@@ -58,12 +70,27 @@ namespace NinetyNineLuftBalloons
             }
             createPascalsTriangle();
 
+            // LINQ example, pretty unneccesary here but prints any coordinate that contains "1"
+            var findCoord = from c in coordList
+                            where c.Contains("1")
+                            select $"These coordinates contain a 1: {c}";
 
-            //Expression-bodied method example
+            Console.SetCursorPosition(20, 2);
+            foreach (string s in findCoord)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+
+
+
+
+            // Expression-bodied method example cause they are neat
             bool is99(int n) => n == 99;
 
 
-            // Pascal's Triangle algorithm just because it is often part of coding tests
+            // Pascal's Triangle algorithm just because it is often part of coding tests, so why not
             void createPascalsTriangle()
             {
                 int rows = 10;
